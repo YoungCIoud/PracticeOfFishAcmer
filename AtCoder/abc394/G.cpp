609135d59e8cd0778d954a16d711126307eb5080
YoungCIoud <YOUNG.050330.cdut@outlook.com>
2025-02-27 21:27:04 +0800

    G 题的新做法

--- AtCoder/abc394/G.cpp
+++ AtCoder/abc394/G.cpp
@@ -14,7 +14,143 @@ using u64 = unsigned long long;
 using i128 = __int128_t;
 
 constexpr i64 Mod = 998244353;
-constexpr int N = 500 * 500, M = 1e6, Inf = 1e9, L = 20;
+// 生成树
+
+// constexpr int N = 500 * 500, M = 1e6, Inf = 1e9, L = 16;
+
+// int fa[N];
+// int get(int u) {
+//     return u == fa[u] ? u : (fa[u] = get(fa[u]));
+// }
+// bool merge(int u, int v) {
+//     int fu = get(u), fv = get(v);
+//     fa[fv] = fu;
+//     return fu != fv;
+// }
+
+// std::vector<std::array<int, 2>> g[N];
+// std::vector st(N, std::vector(L, std::array<int, 2>{})); 
+// std::vector deep(N, 0);
+// void dfs(int cur) {
+//     for (int l = 1; (1 << l) <= deep[cur]; l++) {
+//         st[cur][l][0] = st[st[cur][l - 1][0]][l - 1][0];
+//         st[cur][l][1] = std::min(st[cur][l - 1][1], st[st[cur][l - 1][0]][l - 1][1]);
+//     }
+
+//     for (auto &[to, val] : g[cur]) {
+//         if (to == st[cur][0][0]) {
+//             continue;
+//         }
+
+//         st[to][0] = { cur, val };
+//         deep[to] = deep[cur] + 1;
+//         dfs(to);
+//     }
+// }
+
+// int LCA(int u, int v) {
+//     if (deep[u] != deep[v]) {
+//         if (deep[u] > deep[v]) {
+//             std::swap(u, v);
+//         }
+//         for (int i = L; i >= 0; i--) {
+//             if (deep[v] - (1 << i) >= deep[u]) {
+//                 v = st[v][i][0];
+//             }
+//         }
+//     }
+
+//     if (u == v) {
+//         return u;
+//     }
+
+//     for (int i = L; i >= 0; i--) {
+//         if ((1 << i) <= deep[u] && st[u][i][0] != st[v][i][0]) {
+//             u = st[u][i][0];
+//             v = st[v][i][0];
+//         }
+//     }
+//     return st[u][0][0];
+// }
+
+// int minval(int u, int v) {
+//     if (deep[u] > deep[v]) {
+//         std::swap(u, v);
+//     }
+//     int res = Inf;
+//     for (int d = deep[v] -  deep[u], l = 0; d; d >>= 1, l++) {
+//         if (d & 1) {
+//             res = std::min(res, st[v][l][1]);
+//             v = st[v][l][0];
+//         }
+//     }
+//     return res;
+// }
+
+// void solve()
+// {
+//     int h = 0, w = 0;
+//     std::cin >> h >> w;
+//     std::vector f(h, std::vector(w, 0));
+//     for (int i = 0; i < h; i++) {
+//         for (int j = 0; j < w; j++) {
+//             std::cin >> f[i][j];
+//         }
+//     }
+
+//     // 分离边
+//     std::vector<std::array<int, 3>> edge;
+//     for (int i = 0; i < h; i++) {
+//         for (int j = 1; j < w; j++) {
+//             int u = i * w + j;
+//             edge.push_back({ u - 1, u, std::min( f[i][j], f[i][j - 1]) });
+//         }
+//     }
+//     for (int j = 0; j < w; j++) {
+//         for (int i = 1; i < h; i++) {
+//             int u = i * w + j;
+//             edge.push_back({ u - w, u, std::min( f[i][j], f[i - 1][j]) });
+//         }
+//     }
+
+//     // 生成树的边
+//     std::sort(edge.begin(), edge.end(), [](auto &u, auto &v) {
+//         return u[2] > v[2];
+//     });
+//     for (int i = 0; i < h * w; i++) {
+//         fa[i] = i;
+//     }
+//     for (auto &[u, v, val] : edge) {
+//         if (merge(u, v)) {
+//             g[u].push_back({ v, val });
+//             g[v].push_back({ u, val });
+//         }
+//     }
+
+//     // 生成树上维护 lca 和 st 表
+//     dfs(0);
+
+//     int q = 0;
+//     std::cin >> q;
+//     while (q--) {
+//         int a = 0, b = 0, x = 0;
+//         int c = 0, d = 0, z = 0;
+//         std::cin >> a >> b >> x >> c >> d >> z;
+//         a--, b--, c--, d--;
+
+//         int u = a * w + b, v = c * w + d;
+        
+//         int lca = LCA(u, v);
+        
+//         std::cout << x + z - 2 * std::min({ x, z, minval(u, lca), minval(v, lca) }) << '\n';
+//     }
+//     return;
+// }
+
+
+// 二分
+
+constexpr int N = 500 * 500, M = 1e6, Inf = 1e9;
 
 int fa[N];
 int get(int u) {
@@ -24,65 +160,6 @@ bool merge(int u, int v) {
     int fu = get(u), fv = get(v);
     fa[fv] = fu;
     return fu != fv;
-}
-
-std::vector<std::array<int, 2>> g[N];
-std::vector st(N, std::vector(L, std::array<int, 2>{})); 
-std::vector deep(N, 0);
-void dfs(int cur) {
-    for (int l = 1; (1 << l) <= deep[cur]; l++) {
-        st[cur][l][0] = st[st[cur][l - 1][0]][l - 1][0];
-        st[cur][l][1] = std::min(st[cur][l - 1][1], st[st[cur][l - 1][0]][l - 1][1]);
-    }
-
-    for (auto &[to, val] : g[cur]) {
-        if (to == st[cur][0][0]) {
-            continue;
-        }
-
-        st[to][0] = { cur, val };
-        deep[to] = deep[cur] + 1;
-        dfs(to);
-    }
-}
-
-int LCA(int u, int v) {
-    if (deep[u] != deep[v]) {
-        if (deep[u] > deep[v]) {
-            std::swap(u, v);
-        }
-        for (int i = L; i >= 0; i--) {
-            if (deep[v] - (1 << i) >= deep[u]) {
-                v = st[v][i][0];
-            }
-        }
-    }
-
-    if (u == v) {
-        return u;
-    }
-
-    for (int i = L; i >= 0; i--) {
-        if ((1 << i) <= deep[u] && st[u][i][0] != st[v][i][0]) {
-            u = st[u][i][0];
-            v = st[v][i][0];
-        }
-    }
-    return st[u][0][0];
-}
-
-int minval(int u, int v) {
-    if (deep[u] > deep[v]) {
-        std::swap(u, v);
-    }
-    int res = Inf;
-    for (int d = deep[v] -  deep[u], l = 0; d; d >>= 1, l++) {
-        if (d & 1) {
-            res = std::min(res, st[v][l][1]);
-            v = st[v][l][0];
-        }
-    }
-    return res;
 }
 
 void solve()
@@ -110,38 +187,61 @@ void solve()
             edge.push_back({ u - w, u, std::min( f[i][j], f[i - 1][j]) });
         }
     }
-
-    // 生成树的边
     std::sort(edge.begin(), edge.end(), [](auto &u, auto &v) {
         return u[2] > v[2];
     });
-    for (int i = 0; i < h * w; i++) {
-        fa[i] = i;
-    }
-    for (auto &[u, v, val] : edge) {
-        if (merge(u, v)) {
-            g[u].push_back({ v, val });
-            g[v].push_back({ u, val });
-        }
-    }
-
-    // 生成树上维护 lca 和 st 表
-    dfs(0);
+    // int mx = edge[0][2];
 
     int q = 0;
     std::cin >> q;
-    while (q--) {
-        int a = 0, b = 0, x = 0;
-        int c = 0, d = 0, z = 0;
-        std::cin >> a >> b >> x >> c >> d >> z;
-        a--, b--, c--, d--;
-
-        int u = a * w + b, v = c * w + d;
-        
-        int lca = LCA(u, v);
-        
-        std::cout << x + z - 2 * std::min({ x, z, minval(u, lca), minval(v, lca) }) << '\n';
+    std::vector A(q, 0), B = A, Y = A, C = A, D = A, Z = A, L(q, 1), R(q, M);
+    for (int i = 0; i < q; i++) {
+        std::cin >> A[i] >> B[i] >> Y[i] >> C[i] >> D[i] >> Z[i];
+        A[i]--, B[i]--, C[i]--, D[i]--;
     }
+
+    std::vector mid(M + 1, std::vector<int>{});
+    while (true) {
+        for (int i = 1; i <= M; i++) {
+            mid[i].clear();
+        }
+        bool ok = false;
+        for (int i = 0; i < q; i++) {
+            if (L[i] <= R[i]) {
+                ok = true;
+                mid[L[i] + R[i] >> 1].push_back(i);
+            }
+        }
+        if (not ok) {
+            break;
+        }
+
+        for (int i = 0; i < h * w; i++) {
+            fa[i] = i;
+        }
+        for (int i = M, j = 0; i >= 1; i--) {
+            if (mid[i].empty()) {
+                continue;
+            }
+            while (j < edge.size() && edge[j][2] >= i) {
+                merge(edge[j][0], edge[j][1]);
+                j++;
+            }
+            for (auto &id : mid[i]) {
+                if (get(A[id] * w + B[id]) == get(C[id] * w + D[id])) {
+                    L[id] = i + 1;
+                }
+                else {
+                    R[id] = i - 1;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < q; i++) {
+        std::cout << Y[i] + Z[i] - 2 * std::min({ R[i], Y[i], Z[i] }) << '\n';
+    }
+
     return;
 }
 
